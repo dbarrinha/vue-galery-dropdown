548f4314d2d0bb6a7bf34498a4f306bb70eb295e
dbarrinha <dbarrinha@hotmail.com>
2020-03-06 17:00:22 -0300

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ListDrop @label='teste'/>
+  </div>
+</template>
+
+<script>
+import ListDrop from './components/list-dropdown'
+
+export default {
+  name: 'App',
+  components: {
+    ListDrop
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/list-dropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="react-list-drop__container">
+      <button @click="toggleComponent(!active)" :class="classButtom">{{label}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListDrop',
+  data () {
+    return {
+      active: false,
+      classButtom: `react-list-drop__button ${(this.active ? 'active' : '')}`
+    }
+  },
+  props: {
+    label
+  },
+  methods: {
+    toggleComponent (active) {
+      this.active = active
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
